04eba9e5eb9d0faaf91c7acc5f64429e4036efe9
S.sravan kumar <41701102+sravan1947@users.noreply.github.com>
2019-12-26 12:55:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
-app.use(express.static('public'))
+app.use(express.static('Public'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/Public/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
